168e99de29eed6ec3d719498647e88558f0eb78f
Miguel Angelico Fermin <miguel.fermin@whitecloak.com>
2016-09-03 10:22:42 +0800

    Release 0.1.27

--- scripts/push.sh
+++ scripts/push.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo 'Updating rvm...'
-#source ~/.rvm/scripts/rvm
-#rvm use default
+source ~/.rvm/scripts/rvm
+rvm use default
 echo 'Pushing to pod trunk...'
-#pod trunk push
+pod trunk push
